a31535e44eab23e947d62852ad5b181141b3145a
TMPro <thibault.marie.pro@gmail.com>
2023-10-03 13:15:16 +0200

    tested nuxt routing

--- nuxt-test/app.vue
+++ nuxt-test/app.vue
@@ -1,5 +1,18 @@
 <template>
+
   <div>
-    OHAYO
+    <h1>OHAYO</h1>
   </div>
+<header>
+  <ul>
+    <li>
+      <NuxtLink to="/">HOME</NuxtLink>
+      <NuxtLink to="/events">Events</NuxtLink>
+    </li>
+  </ul>
+</header>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
+
 </template>
---
+++ nuxt-test/pages/events/[id].vue
@@ -0,0 +1,17 @@
+<!-- <script>
+    console.log('ola')
+    const route = useRoute();
+    console.log(route.params)
+</script> -->
+
+<script>
+    const nuxt = useNuxtApp();
+    console.log(nuxt)
+</script>
+
+<template>
+    <div>
+        events id : {{ $route.params.id }}
+    </div>
+</template>
+
